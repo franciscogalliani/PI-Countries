5dc8b304d5b6845cd3218f9129cfae9c09c83c64
franciscogalliani <franemi2k22@gmail.com>
2023-05-08 16:04:04 -0300

    Country model completed

--- PI-Countries-main/api/src/models/Country.js
+++ PI-Countries-main/api/src/models/Country.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    ID: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+      unique: true
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    continent: {
+      type: DataTypes.ENUM(['North America','Americas','South America','Europe','Asia','Africa','Oceania','Antarctic']),
+      allowNull: false
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    subRegion: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    area: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false
   });
 };
