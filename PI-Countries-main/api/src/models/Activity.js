9fa78636e69921d145517eda836d3c6f8e62a4d6
franciscogalliani <franemi2k22@gmail.com>
2023-05-08 19:02:00 -0300

    Models updated

---
+++ PI-Countries-main/api/src/models/Activity.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+    sequelize.define('activity', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            unique: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dificulty: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1,
+                max: 5
+            }
+        },
+        duration: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        season: {
+            type: DataTypes.ENUM(['Summer','Autumn','Winter','Spring'])
+        }
+    },
+    {
+        timestamps: false
+    })
+};
