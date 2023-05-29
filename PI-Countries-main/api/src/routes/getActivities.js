a5ef423d52c2cbe8e153371c51f56aac5be18ff9
franciscogalliani <franemi2k22@gmail.com>
2023-05-29 20:32:21 -0300

    back completed

--- PI-Countries-main/api/src/routes/getActivities.js
+++ PI-Countries-main/api/src/routes/getActivities.js
@@ -5,7 +5,7 @@ const getActivitiesDB = require('../controllers/getActivitiesDB');
 getActivities.get('/activities', async (req, res) => {
     try {
         let activities = await getActivitiesDB();
-        if(activities) res.status(200).json(activities);
+        if(activities.length) res.status(200).json(activities);
         else throw new Error('There is no activities yet');
     } catch (error) {
         res.status(400).send(error.message);
